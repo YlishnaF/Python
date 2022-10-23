feaa0c242919a8caccc854a4a71b435732d64fc9
YlishnaF <ylishnaf@yandex.ru>
2022-10-23 20:28:15 +0500

    hw4

--- firstproject.py
+++ firstproject.py
@@ -1,45 +1,130 @@
-#------------------------------Сумма нечетных чисел------------------------------
-# mas = [4,3,2,8,5,2,0]
-# sum = 0
-# for i in range (len(mas)):
-#     if(i%2==0):
-#         sum+=mas[i]
-# print('Сумма элементов на нечетных позициях: ', sum, "для массива ", mas)
-#------------------------------Произведение пар чисел списка------------------------------
-# mas = [2, 3, 4, 5, 6]
-# print('Массив ', mas)
-# result = []
-# while (len(mas)!=0):
-#     if len(mas)==1:
-#         result.append(mas[0]**2)
-#         break
-#     result.append(mas.pop(0)*mas.pop(-1))
-# print("Произведение пар чисел для массива равно ", result)
-#------------------------------Разница между макс и мин значением дробной части элементов------------------------------
-# mas = [1.1, 1.2, 3.1, 5, 10.01]
-# masCor= []
-# print('Массив: ', mas)
+#-----------------------------------Простые множители------------------------------
+# def isSimpleNum(num):
+#     k=2
+#     while k!=num:
+#         if(num%k==0):
+#             return False
+#         k+=1
+        
+#     return True   
+
+# try:
+#     num = int(input('Введите натуральное число '))
+#     num>0
+# except:
+#     print('Неверно введено число!')
+# if num==1:
+#     print("Введено 1, 1 не является простым числом. Завершение программы")
+# mas = []
+# k=2
+# while num!=1:
+#     if(num%k==0):
+#         if(isSimpleNum(k)):
+#             mas.append(k)
+#             num/=k
+#     else:
+#         k+=1
+# print('Простые множители введеного числа: ', mas )
+#------------------------------Неповторяющиеся элементы------------------------------
+# from random import randint
+# from unittest import result
+# def masCreator(x):
+#     mas=[[0] for i in range(x)]
+    
+#     for i in range(len(mas)):
+#         mas[i]=randint(0, 9)
+            
+#     return mas
+
+# mas=masCreator(10)
+# print('Изначальный массив: ', mas)
+# result=[]
 # for i in range(len(mas)):
-#     k = round(mas[i]%1, 7)
-#     if(k!=0):
-#          masCor.append(round(mas[i]%1, 9))
-# max=masCor[0]
-# min=masCor[0]
-# for i in range(1,len(masCor)):
-#     if(max<masCor[i]): max=masCor[i]
-#     if(min>masCor[i]): min=masCor[i]
-# print('Разница: ',max-min)
-#------------------------------Преобразовывать десятичное число в двоичное------------------------------
-n=1175
-print('Число для перевода в двоичную систему ', n)
-print('Представдение в двоичной системе числа c использованием встроенной функции ', bin(n))  
-dNum = []
-while n!=0:
-    if n%2!=0:
-        dNum.append(1)
-        n=n//2
-    else:
-        dNum.append(0)
-        n=n//2
-      
-print('Представдение в двоичной системе числа', "".join(map(str, dNum[::-1])))
+#     if(mas[i] in result):
+#         continue
+#     else:
+#         result.append(mas[i])
+# print('Уникальные элементы в массиве: ', result)
+
+#------------------------------Корни квадратного уравнения------------------------------
+# import re
+# equation = input('Введите квадратное уравнение в виде: ax2+bx+c=0. Укажите все коэффициенты, даже если они равны 1 или 0: ')
+# regex = re.compile(r'(-?\d+)x2([+-]\d+)x([+-]\d+)=0')
+# matches = regex.match(equation)
+# if matches is None:
+#     print('Неверно введено уравнение')
+#     exit()
+# a = int(matches.group(1))
+# b = int(matches.group(2))
+# c = int(matches.group(3))
+# D = b**2 - 4*a*c
+# if(D>0):
+#     x1=(-b + D**0.5)/(2*a)
+#     x2=(-b - D**0.5)/(2*a)
+#     print("Первый корень:  ", x1)
+#     print("Второй корень:  ", x2)
+# elif(D==0):
+#      x1=-b+(D)**0.5/2*a
+#      print("Один корень:  ", x1)
+# else:
+#     print('Корней нет')
+# ---------------------------------------------Чат бот-------------------------------------------------
+import json
+from secrets import choice
+data=[]
+
+def save():
+    with open("library.json", "w+", encoding="utf-8") as file:
+        file.write(json.dumps(data, ensure_ascii=False))
+
+try:
+    with open("library.json", "r", encoding="utf-8") as file:
+        data = json.load(file) 
+except:
+    data = {'Джоан Роулинг': ['Гарри Поттер и кубок огня', 'Гарри Поттер и тайная комната'], 'Лев Толстой':['Анна Каренина', 'Война и мир']}
+    save() 
+print(data)
+
+print('Добро пожаловать, введите /start для начала работы ')
+command = input()
+if command=="/start":
+    while True:    
+        print('Введите /all для просмотра всей библиотеки,\n /author для просмотра всех книг автора и /exit для выхода')
+        response = input('Введите команду ')
+        if(response=='/all'):
+            print(data)
+        elif(response=="/author"):
+            print('Введите имя автора из вашей библиотеки для просмотра всех его книг')
+            author=input('Имя автора ')
+            if(author not in data):
+                print("У вас нет такого автора")
+                continue
+            print(data.get(author))
+            print('Если вы хотите добавть книгу для данного автора, введите /append, \n/delete, ecли хотите ее удалить и /random для получения любой книги автора из библиотеки')
+            command=input()
+            if(command=='/append'):
+                try:
+                    command=input('Название книги ')
+                    data[author].append(command)
+                    print('Книга успешно добавлена, список книг автора: ', data.get(author))
+                    save()
+                except:
+                    print('Нет такой книги в библиотеке')
+            elif(command=='/delete'):
+                try:
+                    command=input('Название ')
+                    data[author].remove(command)
+                    print('Книга успешно улалена, список книг автора: ', data.get(author))
+                    save()
+                except:
+                    print('Нет такой книги в библиотеке')
+            elif(command=="/random"):
+                print("Сегодня читаем прекраную книгу: ",choice(data.get(author)))
+            
+            else:
+                print('Введена неверная комманда, попробуйте еще раз!')
+        elif(response=="/exit"):
+                print('Выходим их библиотеки, будем рады видеть вас снова!')
+                exit()
+else:
+     print('Введена неверная комманда, попробуйте еще раз!')
